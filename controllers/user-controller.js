87dcddbf69c776c4cfeab8bdb6a424e93e547ba0
Makda Girmay <makdagirmay4@gmail.com>
2022-01-17 16:16:47 -0500

    set up server and created user model with working routes

---
+++ controllers/user-controller.js
@@ -0,0 +1,56 @@
+const { User } = require('../models');
+
+const userController = {
+    //get all users
+    getAllUser(req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    //get one user by id
+    getUserById({ params }, res){
+        User.findOne({ _id: params.id })
+        .then(dbUserData => res.json(dbUserData))
+        .cathc(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    //create new user
+    createUser({ body },res){
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err =>res.json(err));
+    },
+
+    updateUser({ params, body }, res){
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true})
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({message: 'no user with that ID'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    deleteUser({ params }, res){
+        User.findOneAndDelete({_id: params.id})
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({message: 'No User with this id found' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = userController;
